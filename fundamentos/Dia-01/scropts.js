e7955e6909a79e3ea673dc2fccbcf9613258cdd6
cristianemiranda <cris.cristianemiranda@gmail.com>
2023-01-26 10:49:48 -0300

    exercicio

--- fundamentos/Dia-01/scropts.js
+++
@@ -1,9 +0,0 @@
-const myName = "Cristiane";
-const birthCity = "Jacarezinho";
-birthCity = "Cambará"
-let birthYear = 1989;
-birthYear = 2030
-
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
