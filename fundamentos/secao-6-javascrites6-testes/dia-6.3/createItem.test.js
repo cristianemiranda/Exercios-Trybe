b8de132b58da8e54f92d0dc7c96057d203dcb018
Cristiane <cris.cristianemiranda@gmail.com>
2023-02-27 19:12:35 -0300

    Exercicios-dia-6.3

---
+++ fundamentos/secao-6-javascrites6-testes/dia-6.3/createItem.test.js
@@ -0,0 +1,28 @@
+const createItem = require('./createItem');
+describe('a função createItem', () => {
+  it('cria um item válido', () => {
+    const objetos = {name: 'banana', unit: 'kg', price: 1.99, quantity:20};
+    const atual = createItem('banana', 'kg', 1.99,20);
+    expect(atual).toEqual(objetos);
+  });
+  it('utiliza zero como quantidade padrão', () => {
+    const objetos = 0;
+    const atual = createItem('banana', 'kg', 1.99);
+    expect(atual).toHaveProperty('quantity', objetos);
+  });
+   it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => createItem()).toThrow();
+   });
+   it('Lança um erro se o nome do item não é uma string', () => {
+    expect(() => createItem(1.99, 'kg', 'banana', 20))
+    .toThrow('O nome do item deve ser uma string');
+   });
+   it('Lança um erro se o preço é negativo', () => {
+    expect(() => createItem('banana', 'kg', -0.01))
+    .toThrow('O preço do item deve ser maior que zero');
+   });
+   it('Lança um erro se o preço é zero', () => {
+    expect(() => createItem('banana', 'kg', 0.00))
+    .toThrow('O preço do item deve ser maior que zero');
+ })
+});
